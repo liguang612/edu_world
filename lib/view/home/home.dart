d7b1f0025fb0c1bef23e0aaa9eb937e37ad65133
Ragnie <p.lyquangk.lythedan@gmail.com>
2025-03-24 00:58:00 +0700

    Cloud firestore handmade tool

---
+++ lib/view/home/home.dart
@@ -0,0 +1,19 @@
+import 'package:edu_world/utils/cloud_firestore_tools.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () {
+              CloudFirestoreTools.startTools();
+            },
+            child: const Text('START')),
+      ),
+    );
+  }
+}
